ba6eaa3b28828bf2d716d5e2b2243c6f93176626
Sai Prasad Reddy Kukudala <kukudalasaiprasad@gmail.com>
2025-05-01 23:37:01 -0500

    Added Burn function in ReputationToken.sol and further modified its test file

--- contracts/ReputationToken.sol
+++ contracts/ReputationToken.sol
@@ -2,27 +2,27 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract ReputationToken is ERC20, Ownable {
+contract ReputationToken is ERC20, AccessControl {
+    bytes32 public constant MINTER_BURNER_ROLE = keccak256("MINTER_BURNER_ROLE");
+
     constructor(string memory name, string memory symbol, address initialOwner)
         ERC20(name, symbol)
-        Ownable(initialOwner)
-    {}
+    {
+        _grantRole(DEFAULT_ADMIN_ROLE, initialOwner);
+        _grantRole(MINTER_BURNER_ROLE, initialOwner);
+    }
 
     function mint(address to, uint256 amount) external {
-        require(msg.sender == owner(), "Only owner can call this function");
+        require(hasRole(MINTER_BURNER_ROLE, msg.sender), "Caller is not a minter");
         require(to != address(0), "ERC20: mint to the zero address");
         _mint(to, amount);
     }
 
-    function _update(address from, address to, uint256 amount) internal override {
-        if (to == address(0)) {
-            revert("ERC20: transfer to the zero address");
-        }
-        if (from != address(0) && balanceOf(from) < amount) {
-            revert("ERC20: transfer amount exceeds balance");
-        }
-        super._update(from, to, amount);
+    function burn(address from, uint256 amount) external {
+        require(hasRole(MINTER_BURNER_ROLE, msg.sender), "Caller is not a burner");
+        require(from != address(0), "ERC20: burn from the zero address");
+        _burn(from, amount);
     }
 }
